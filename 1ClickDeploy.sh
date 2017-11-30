e772f0bd9cf1d4be6e0c75a0e0f647386eb33890
maxzh0916 <33621127+maxzh0916@users.noreply.github.com>
2017-11-30 19:26:25 +0800

    Update 1ClickDeploy.sh

--- 1ClickDeploy.sh
+++ 1ClickDeploy.sh
@@ -4,7 +4,7 @@
 install_ssr(){
 	cd /root/
 	echo '下载ssr'
-	git clone git://github.com/maxzh0916/1ClickDeploy.git && cd 1ClickDeploy && chmod +x setup_cymysql.sh && chmod +x ./initcfg.sh && ./setup_cymysql.sh && ./initcfg.sh
+	git clone -b https://github.com/maxzh0916/1ClickDeploy.git && cd 1ClickDeploy && chmod +x setup_cymysql.sh && chmod +x ./initcfg.sh && ./setup_cymysql.sh && ./initcfg.sh
 	echo 'ssr安装完成'
 	stty erase '^H' && read -p " API接口（mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, glzjinmod, legendsockssr）:" ssapi
 	stty erase '^H' && read -p " mysql服务器地址:" ssserver
