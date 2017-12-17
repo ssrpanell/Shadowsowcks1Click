25bb15bc4915489887ac1e18c36d1f696eb0dc10
maxzh0916 <33621127+maxzh0916@users.noreply.github.com>
2017-12-17 20:31:19 +0800

    1.增加定时重启节点
    2.更新libsodium
    3.优化脚本

--- Shadowsowcks1Click.sh
+++ Shadowsowcks1Click.sh
@@ -1,17 +1,7 @@
-#Require Root Permission
+#!/bin/bash
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 install_ssr(){
-	cd /root/
-  	wget https://github.com/jedisct1/libsodium/releases/download/1.0.15/libsodium-1.0.15.tar.gz
-  	tar xf libsodium-1.0.15.tar.gz && cd libsodium-1.0.15
-  	./configure && make -j2 && make install
-  	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-  	cd /root/
-  	rm -rf libsodium-1.0.15.tar.gz
-	echo 'libsodium安装完成'
-  	git clone -b master https://github.com/maxzh0916/Shadowsowcks1Click.git && mv Shadowsowcks1Click shadowsocksr && cd shadowsocksr && chmod +x setup_cymysql.sh && chmod +x ./initcfg.sh && ./setup_cymysql.sh && ./initcfg.sh
-	rm -rf Shadowsowcks1Click.sh
-	echo 'ssr安装完成'
+	clear
 	stty erase '^H' && read -p " API接口（mudbjson, sspanelv2, sspanelv3, sspanelv3ssr, glzjinmod, legendsockssr）:" ssapi
 	stty erase '^H' && read -p " mysql服务器地址:" ssserver
 	stty erase '^H' && read -p " mysql服务器端口:" ssport
@@ -22,6 +12,18 @@ install_ssr(){
 	stty erase '^H' && read -p " 加密（method）:" ssmethod
 	stty erase '^H' && read -p " 协议（protocol）:" ssprotocol
 	stty erase '^H' && read -p " 混淆（obfs）:" ssobfs
+	clear
+	cd /root/
+  	wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+  	tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+  	./configure && make -j2 && make install
+  	echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+  	cd /root/
+  	rm -rf libsodium-1.0.16.tar.gz
+	echo 'libsodium安装完成'
+  	git clone -b master https://github.com/maxzh0916/Shadowsowcks1Click.git && mv Shadowsowcks1Click shadowsocksr && cd shadowsocksr && chmod +x setup_cymysql.sh && chmod +x ./initcfg.sh && ./setup_cymysql.sh && ./initcfg.sh
+	rm -rf Shadowsowcks1Click.sh
+	echo 'ssr安装完成'
 	sed -i -e "s/ssapi/$ssapi/g" userapiconfig.py
 	sed -i -e "s/ssserver/$ssserver/g" usermysql.json
 	sed -i -e "s/ssport/$ssport/g" usermysql.json
@@ -38,40 +40,40 @@ install_ssr(){
 	echo 'ssr已开始运行'
 	service iptables stop
 	service firewalld stop
-	echo '暂时关闭iptables、firewalld，如有需求请自行配置。'
+	systemctl disable firewalld.service
+	chkconfig iptables off
+	echo '已关闭iptables、firewalld，如有需要请自行配置。'
 }
 
 open_bbr(){
+	clear
 	cd
 	wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
 	chmod +x bbr.sh
 	./bbr.sh
-
 }
 
-if [ `rpm -qa | git |wc -l` -ne 0 ];then
-	echo "已安装git" 
-else
-	if [ -f /etc/redhat-release ]; then
-	yum -y install git
-	elif cat /etc/issue | grep -Eqi "debian"; then
-	apt-get -y install git
-	elif cat /etc/issue | grep -Eqi "ubuntu"; then
-	apt-get -y install git
-	elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-	yum -y install git
-	elif cat /proc/version | grep -Eqi "debian"; then
-	apt-get -y install git
-	elif cat /proc/version | grep -Eqi "ubuntu"; then
-	apt-get -y install git
-	elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-	yum -y install git
-	fi
-fi
-echo ' 1. 部署SSR远程节点'
-echo ' 2. 安装BBR'
-echo ' yum -y groupinstall "Development Tools、apt-get install build-essential'
-stty erase '^H' && read -p " 请输入数字 [1-2]:" num
+auto_reboot(){
+	clear
+	echo '设置每天几点几分重启节点'
+	stty erase '^H' && read -p " 小时(0-23):" hour
+	stty erase '^H' && read -p " 分钟(0-59):" minute
+	chmod +x /etc/rc.d/rc.local
+	echo /sbin/service crond start >> /etc/rc.d/rc.local
+	echo "/root/shadowsocksr/run.sh" >> /etc/rc.d/rc.local
+	echo '设置开机运行SSR'
+	echo "$minute $hour * * * root /sbin/reboot" >> /etc/crontab
+	service crond start
+}
+
+yum -y install git
+yum -y groupinstall "Development Tools"
+clear
+echo ' 注意：此脚本基于centos7编写，其他系统可能会出问题'
+echo ' 1. 安装 SSR'
+echo ' 2. 安装 BBR'
+echo ' 3. 设置定时重启（测试中）'
+stty erase '^H' && read -p " 请输入数字 [1-3]:" num
 case "$num" in
 	1)
 	install_ssr
@@ -79,7 +81,10 @@ case "$num" in
 	2)
 	open_bbr
 	;;
+	3)
+	auto_reboot
+	;;
 	*)
-	echo '请输入正确数字 [1-2]'
+	echo '请输入正确数字 [1-3]'
 	;;
 esac
